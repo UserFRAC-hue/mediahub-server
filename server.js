65af05e4b2fcbfb3c44dad1b34441d3d1fc0974a
UserFRAC-hue <diegoaxaxazaz@gmail.com>
2026-06-02 13:13:05 -0500

    Update server.js

--- server.js
+++ server.js
@@ -157,5 +157,3 @@ app.get('/metricas/instagram/:ig_id', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`MediaHub server en puerto ${PORT}`));
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`MediaHub server en puerto ${PORT}`));
